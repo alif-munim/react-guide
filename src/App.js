62b10bfc66e134dce8276f543f39f365edb05e38
Alif Munim <alif.munim@ryerson.ca>
2020-03-01 00:40:32 -0500

    create a simple counter; affect state with increment and decrement buttons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement } from "./actions";
 
 function App() {
+  const counter = useSelector(state => state.counter);
+  const isLogged = useSelector(state => state.isLogged);
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Counter {counter}</h1>
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement(5))}>-</button>
+      {isLogged ?  <h3>Logged Dashboard</h3> : ""}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,43 +4,23 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { createStore } from "redux";
+import allReducers from "./reducers";
+import { Provider } from "react-redux";
+
+const store = createStore(
+    allReducers,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 
-// Action: action to be performed
-const increment = () => {
-    return {
-        type: "INCREMENT"
-    }
-}
-
-const decrement = () => {
-    return {
-        type: "DECREMENT"
-    }
-}
-
-// Reducer: how actions transform state
-
-const counter = (state = 0, action) => {
-    switch(action.type) {
-        case "INCREMENT":
-            return state + 1;
-        case "DECREMENT":
-            return state - 1;
-    }
-}
-
-// Store: globalized state
-
-let store = createStore(counter);
-store.subscribe(() => console.log(store.getState()));
-
-// Dispatch: send action to reducer
-
-store.dispatch(increment());
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
