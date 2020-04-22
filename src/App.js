4fbd2f76fa691740ca9b0a6cd6bfa3e8d3ce69cc
Alif Munim <alif.munim@ryerson.ca>
2020-04-22 13:30:12 -0400

    Add dropdown display logic using state, style dropdown items

--- src/App.js
+++ src/App.js
@@ -1,14 +1,66 @@
-import React from "react";
+import { ReactComponent as BellIcon } from "./icons/bell.svg";
+import { ReactComponent as MessengerIcon } from "./icons/messenger.svg";
+import { ReactComponent as CaretIcon } from "./icons/caret.svg";
+import { ReactComponent as PlusIcon } from "./icons/plus.svg";
+import { ReactComponent as CogIcon } from "./icons/cog.svg";
+import { ReactComponent as ChevronIcon } from "./icons/chevron.svg";
+import { ReactComponent as ArrowIcon } from "./icons/arrow.svg";
+import { ReactComponent as BoltIcon } from "./icons/bolt.svg";
+
+import React, { useState } from "react";
 
 function App() {
-  return <Navbar></Navbar>;
+  return (
+    <Navbar>
+      <NavItem icon={<PlusIcon />}></NavItem>
+      <NavItem icon={<BellIcon />}></NavItem>
+      <NavItem icon={<MessengerIcon />}></NavItem>
+
+      <NavItem icon={<CaretIcon />}>
+        <DropdownMenu></DropdownMenu>
+      </NavItem>
+    </Navbar>
+  );
 }
 
-function Navbar() {
+function Navbar(props) {
   return (
     <nav className="navbar">
-      <ul className="navbar-nav"></ul>
+      <ul className="navbar-nav">{props.children}</ul>
     </nav>
+  );
+}
+
+function NavItem(props) {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+        {props.icon}
+      </a>
+
+      {open && props.children}
+    </li>
+  );
+}
+
+function DropdownMenu() {
+  function DropdownItem(props) {
+    return (
+      <a href="#" className="menu-item">
+        <span className="icon-button">{props.leftIcon}</span>
+        {props.children}
+        <span className="icon-right">{props.rightIcon}</span>
+      </a>
+    );
+  }
+
+  return (
+    <div className="dropdown">
+      <DropdownItem>My Profile</DropdownItem>
+      <DropdownItem leftIcon={<CogIcon />}></DropdownItem>
+    </div>
   );
 }
 
